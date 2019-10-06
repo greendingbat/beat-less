d6c5a78efc6839a55f0aafa557a897131db2bbc5
Ian Glockner <ianglockner@gmail.com>
2019-10-06 00:00:15 -0700

    Event syncing to the downbeat added

---
+++ Assets/Scripts/DownObstacle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownObstacle : MonoBehaviour
+{
+	[SerializeField] private Animator[] animators;
+	[SerializeField] private bool isDown = false;
+
+	void Start()
+	{
+		animators = GetComponentsInChildren<Animator>();
+		foreach (Animator animator in animators)
+		{
+			Debug.Log(animator.gameObject.name);
+			animator.SetBool("is_down", isDown);
+		}
+		BeatLessGameManager.OnDownBeatEnter += ToggleDown;
+	}
+
+	public void ToggleDown()
+	{
+		foreach (Animator animator in animators)
+		{
+			isDown = !isDown;
+			Debug.Log("Setting " + animator.gameObject.name + " is_down to: "+isDown);
+			animator.SetBool("is_down", isDown);
+		}
+	}
+}
---
+++ Assets/Scripts/OpenObstacle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenObstacle : MonoBehaviour
+{
+	[SerializeField] private Animator[] animators;
+	[SerializeField] private bool isOpen = false;
+
+	void Start()
+	{
+		animators = GetComponentsInChildren<Animator>();
+		foreach (Animator animator in animators)
+		{
+			Debug.Log(animator.gameObject.name);
+			animator.SetBool("is_open", isOpen);
+		}
+		BeatLessGameManager.OnDownBeatEnter += ToggleOpen;
+	}
+
+	public void ToggleOpen()
+	{
+		foreach (Animator animator in animators)
+		{
+			isOpen = !isOpen;
+			Debug.Log("Setting " + animator.gameObject.name + " is_open to: " + isOpen);
+			animator.SetBool("is_open", isOpen);
+		}
+	}
+}
