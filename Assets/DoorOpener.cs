d06db6e9890565901ff016716991fadfd58804f4
Matthew Burns <matthewburns@email.arizona.edu>
2019-10-05 20:56:40 -0700

    TIMER WORKSgit pullgit pull

---
+++ Assets/DoorOpener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+	Animator a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (false) {
+			a = GetComponent<Animator>();
+			if (a.GetBool("is_open")) {
+				a.SetBool("is_open", false);
+			} else {
+				a.SetBool("is_open", true);
+			}
+		}
+    }
+}
---
+++ Assets/DoorOpener2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener2 : MonoBehaviour
+{
+	Animator a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = GetComponent<Animator>();
+    }
+
+	public float targetTime = 60f / 95f * 2;
+    // Update is called once per frame
+    void Update()
+    {
+		targetTime -= Time.deltaTime;
+        if (targetTime <= 0.0f) {
+			
+			if (a.GetBool("is_open")) {
+				a.SetBool("is_open", false);
+			} else {
+				a.SetBool("is_open", true);
+			}
+			targetTime = 60f / 95f * 2;
+		}
+    }
+}
